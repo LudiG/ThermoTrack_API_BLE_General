d47d35bb12cfccff1d56820cb90080e40b8fff66
Ludwig Gryffenberg <ludigryff@gmail.com>
2017-10-26 16:45:22 +0200

    Fixed issue with OpenSSL being passed arguments with incompatible formatting.

--- ThermoTrack_API_BLE_General/OpenSSLController.cs
+++ ThermoTrack_API_BLE_General/OpenSSLController.cs
@@ -7,98 +7,91 @@ namespace ThermoTrack_API_BLE_General
 {
     public static class OpenSSLController
     {
-        public static string CreateCertificateRequest(string nameCommon)
+        public static void CreateCertificateRequest(string nameCommon)
         {
-            string error;
-
             string directorySSL = WebConfigurationManager.AppSettings["PATH_SSL"];
+            string logPath = WebConfigurationManager.AppSettings["PATH_LOGS_SSL"];
 
-            Process process = new Process();
+            Debug.WriteLine(directorySSL);
 
-            process.StartInfo.FileName = "cmd.exe";
-            process.StartInfo.UseShellExecute = false;
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.UseShellExecute = false;
 
-            process.StartInfo.Arguments = @"/c openssl req -days 3650 -nodes -new -keyout %KEY_DIR%\%KEY_NAME%.key -out %KEY_DIR%\%KEY_NAME%.csr -config %HOME%\openssl-1.0.0.cnf -batch";
+                process.StartInfo.WorkingDirectory = directorySSL;
 
-            //process.StartInfo.EnvironmentVariables["HOME"] = @"%ProgramFiles%\OpenVPN\easy-rsa";
-            process.StartInfo.EnvironmentVariables["HOME"] = directorySSL;
+                process.StartInfo.Arguments = @"/c openssl req -days 3650 -nodes -new -keyout %KEY_DIR%\%KEY_NAME%.key -out %KEY_DIR%\%KEY_NAME%.csr -config openssl-1.0.0.cnf -batch";
 
-            process.StartInfo.EnvironmentVariables["KEY_DIR"] = directorySSL + @"\keys";
-            process.StartInfo.EnvironmentVariables["KEY_SIZE"] = "1024";
+                process.StartInfo.EnvironmentVariables["HOME"] = directorySSL;
 
-            process.StartInfo.EnvironmentVariables["KEY_COUNTRY"] = "ZA";
-            process.StartInfo.EnvironmentVariables["KEY_PROVINCE"] = "WP";
-            process.StartInfo.EnvironmentVariables["KEY_CITY"] = "CapeTown";
-            process.StartInfo.EnvironmentVariables["KEY_ORG"] = "ThermoTrack";
-            process.StartInfo.EnvironmentVariables["KEY_OU"] = "";
-            process.StartInfo.EnvironmentVariables["KEY_EMAIL"] = "jaco@istm.co.za";
-            process.StartInfo.EnvironmentVariables["KEY_CN"] = nameCommon;
-            process.StartInfo.EnvironmentVariables["KEY_NAME"] = nameCommon;
+                process.StartInfo.EnvironmentVariables["KEY_DIR"] = "keys";
+                process.StartInfo.EnvironmentVariables["KEY_SIZE"] = "1024";
 
-            process.StartInfo.EnvironmentVariables["PKCS11_MODULE_PATH"] = "changeme";
-            process.StartInfo.EnvironmentVariables["PKCS11_PIN"] = "1234";
+                process.StartInfo.EnvironmentVariables["KEY_COUNTRY"] = "ZA";
+                process.StartInfo.EnvironmentVariables["KEY_PROVINCE"] = "WP";
+                process.StartInfo.EnvironmentVariables["KEY_CITY"] = "CapeTown";
+                process.StartInfo.EnvironmentVariables["KEY_ORG"] = "ThermoTrack";
+                process.StartInfo.EnvironmentVariables["KEY_OU"] = "";
+                process.StartInfo.EnvironmentVariables["KEY_EMAIL"] = "jaco@istm.co.za";
+                process.StartInfo.EnvironmentVariables["KEY_CN"] = nameCommon;
+                process.StartInfo.EnvironmentVariables["KEY_NAME"] = nameCommon;
 
-            process.StartInfo.RedirectStandardError = true;
-            process.StartInfo.RedirectStandardInput = true;
-            process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.EnvironmentVariables["PKCS11_MODULE_PATH"] = "changeme";
+                process.StartInfo.EnvironmentVariables["PKCS11_PIN"] = "1234";
 
-            process.Start();
-            process.WaitForExit();
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.RedirectStandardOutput = true;
 
-            error = process.StandardError.ReadToEnd();
-
-            process.Close();
-
-            return error;
+                process.Start();
+                process.WaitForExit();
+            }
         }
 
         public static void SignCertificateRequest(string nameCommon)
         {
-            string error;
-
             string directorySSL = WebConfigurationManager.AppSettings["PATH_SSL"];
+            string logPath = WebConfigurationManager.AppSettings["PATH_LOGS_SSL"];
 
-            Process process = new Process();
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.UseShellExecute = false;
 
-            process.StartInfo.FileName = "cmd.exe";
-            process.StartInfo.UseShellExecute = false;
+                process.StartInfo.WorkingDirectory = directorySSL;
 
-            process.StartInfo.Arguments = @"/c openssl ca -days 3650 -out %KEY_DIR%\%KEY_NAME%.crt -in %KEY_DIR%\%KEY_NAME%.csr -config %HOME%\openssl-1.0.0.cnf -batch";
+                process.StartInfo.Arguments = @"/c openssl ca -days 3650 -out %KEY_DIR%\%KEY_NAME%.crt -in %KEY_DIR%\%KEY_NAME%.csr -config openssl-1.0.0.cnf -batch";
 
-            //process.StartInfo.EnvironmentVariables["HOME"] = @"%ProgramFiles%\OpenVPN\easy-rsa";
-            process.StartInfo.EnvironmentVariables["HOME"] = directorySSL;
+                process.StartInfo.EnvironmentVariables["HOME"] = directorySSL;
 
-            process.StartInfo.EnvironmentVariables["KEY_DIR"] = directorySSL + @"\keys";
-            process.StartInfo.EnvironmentVariables["KEY_SIZE"] = "1024";
+                process.StartInfo.EnvironmentVariables["KEY_DIR"] = "keys";
+                process.StartInfo.EnvironmentVariables["KEY_SIZE"] = "1024";
 
-            process.StartInfo.EnvironmentVariables["KEY_COUNTRY"] = "ZA";
-            process.StartInfo.EnvironmentVariables["KEY_PROVINCE"] = "WP";
-            process.StartInfo.EnvironmentVariables["KEY_CITY"] = "CapeTown";
-            process.StartInfo.EnvironmentVariables["KEY_ORG"] = "ThermoTrack";
-            process.StartInfo.EnvironmentVariables["KEY_OU"] = "";
-            process.StartInfo.EnvironmentVariables["KEY_EMAIL"] = "jaco@istm.co.za";
-            process.StartInfo.EnvironmentVariables["KEY_CN"] = nameCommon;
-            process.StartInfo.EnvironmentVariables["KEY_NAME"] = nameCommon;
+                process.StartInfo.EnvironmentVariables["KEY_COUNTRY"] = "ZA";
+                process.StartInfo.EnvironmentVariables["KEY_PROVINCE"] = "WP";
+                process.StartInfo.EnvironmentVariables["KEY_CITY"] = "CapeTown";
+                process.StartInfo.EnvironmentVariables["KEY_ORG"] = "ThermoTrack";
+                process.StartInfo.EnvironmentVariables["KEY_OU"] = "";
+                process.StartInfo.EnvironmentVariables["KEY_EMAIL"] = "jaco@istm.co.za";
+                process.StartInfo.EnvironmentVariables["KEY_CN"] = nameCommon;
+                process.StartInfo.EnvironmentVariables["KEY_NAME"] = nameCommon;
 
-            process.StartInfo.EnvironmentVariables["PKCS11_MODULE_PATH"] = "changeme";
-            process.StartInfo.EnvironmentVariables["PKCS11_PIN"] = "1234";
+                process.StartInfo.EnvironmentVariables["PKCS11_MODULE_PATH"] = "changeme";
+                process.StartInfo.EnvironmentVariables["PKCS11_PIN"] = "1234";
 
-            process.StartInfo.RedirectStandardError = true;
-            process.StartInfo.RedirectStandardInput = true;
-            process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.RedirectStandardOutput = true;
 
-            process.Start();
-            process.WaitForExit();
-
-            error = process.StandardError.ReadToEnd();
-
-            process.Close();
+                process.Start();
+                process.WaitForExit();
+            }
         }
 
         public static bool MoveCertificateFiles(string nameCommon, string directoryTarget)
         {
             string directorySSL = WebConfigurationManager.AppSettings["PATH_SSL"];
-
             string logPath = WebConfigurationManager.AppSettings["PATH_LOGS_SSL"];
 
             string pathCertficateSource = directorySSL + @"\keys\" + nameCommon + ".crt";
